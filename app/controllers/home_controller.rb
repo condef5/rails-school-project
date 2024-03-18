3d27dc2f5bad08bd30956311a3554468f3d27fe0
Frank Condezo <frankcondezo@gmail.com>
2024-03-17 19:45:05 -0500

    add filters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
-    @books = Book.search(params).includes(:author, :category, :editorial)
+    @books = Book.search(filter_params[:query])
+      .filter_by_category(filter_params[:category_ids])
+      .filter_by_author(filter_params[:author_ids])
+      .filter_by_editorial(filter_params[:editorial_ids])
+      .includes(:author, :category, :editorial)
+  end
+
+  private
+
+  def filter_params
+    params.permit(:query, category_ids: [], author_ids: [], editorial_ids: [])
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,9 +3,10 @@ class Book < ApplicationRecord
   belongs_to :category
   belongs_to :author
 
-  def self.search(params)
-    params[:query].blank? ? all : where(
-      "title LIKE ?", "%#{sanitize_sql_like(params[:query])}%"
-    )
-  end
+  scope :filter_by_author, ->(value) { where(author_id: value) if value.present? }
+  scope :filter_by_editorial, ->(value) { where(editorial_id: value) if value.present? }
+  scope :filter_by_category, ->(value) { where(category_id: value) if value.present? }
+  scope :search, ->(query) {
+    query.blank? ? all : where("title LIKE ?", "%#{sanitize_sql_like(query)}%")
+  }
 end
