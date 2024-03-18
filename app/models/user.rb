d507d217c9fa708ac3fe7c69d627724f1c49bd09
Frank Condezo <frankcondezo@gmail.com>
2024-03-18 05:21:31 -0500

    last fix 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  enum role: { user: 0, admin: 1,  }
+  enum role: { user: 0, admin: 1 }
+  validates :email, presence: true, uniqueness: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
